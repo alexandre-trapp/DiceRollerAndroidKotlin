f1b593599bf7f116b7625906a0203b27b36a9342
Alexandre Cordeiro Trapp <alexandre.trapp@hotmail.com>
2020-06-01 12:15:05 -0300

    add button roll, random number to click button

--- app/src/main/java/com/trapp/diceroller/MainActivity.kt
+++ app/src/main/java/com/trapp/diceroller/MainActivity.kt
@@ -2,11 +2,29 @@ package com.trapp.DiceRollerAndroidKotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.text = "LET'S ROLL"
+        rollButton.setOnClickListener {
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+
+        val resultText: TextView = findViewById(R.id.result_text)
+
+        val randomNumber = Random().nextInt(6) + 1
+
+        resultText.text = randomNumber.toString()
     }
 }
