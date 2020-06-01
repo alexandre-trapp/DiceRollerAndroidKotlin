afd3e2b14a418aecc942b18e43c69f6f2e7babdc
Alexandre Cordeiro Trapp <alexandre.trapp@hotmail.com>
2020-06-01 13:55:12 -0300

    add lateinit, vector to oldest androids

--- app/src/main/java/com/trapp/diceroller/MainActivity.kt
+++ app/src/main/java/com/trapp/diceroller/MainActivity.kt
@@ -9,15 +9,20 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var diceImage: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.roll_button)
+
         rollButton.text = "LET'S ROLL"
         rollButton.setOnClickListener {
             rollDice()
         }
+
+        diceImage = findViewById(R.id.dice_image)
     }
 
     private fun rollDice() {
@@ -33,7 +38,6 @@ class MainActivity : AppCompatActivity() {
             else -> R.drawable.dice_6
         }
 
-        val diceImage: ImageView = findViewById(R.id.dice_image)
         diceImage.setImageResource(drawableResource)
     }
 }
